4e347da5a169b735743dc2045bbc3d3730acdf43
Gauthier7k <98077233+Gauthier7k@users.noreply.github.com>
2025-03-09 20:57:37 -0600

    ex 4

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -1,96 +1,129 @@
-import array
-class Array:
-    def __init__(self,data=[]):
-        self.array=data
-
-    def display(self):
-        print(self.array)
-            
-    def push(self,data):
-        self.array.append(data)
+import random
+import timeit
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-    def pop(self):
-        if len(self.array)<=1:
-            self.array=[]
+class ArrayQueue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+    
+    def dequeue(self):
+        if not self.queue:
             return None
-        self.array.remove(self.array[len(self.array)-1])
-        return self.array[len(self.array)-1]
-      
-        
+        return self.queue.pop()
+
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-    def push(self, value):
-        new_node = Node(value)
-        new_node.next = self
-        self = new_node
-        return new_node 
+class LinkedListQueue:
+    def __init__(self):
+        self.head = None  
+        self.tail = None  
     
-    def pop(self):
+    def enqueue(self, item):
+        new_node = Node(item)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    
 
-        if self.next is None:
-            return None, None
-        return self.next, self.data
-
-    def displayLL(self):
-        if (self.next is None):
-            print("No data")
-            return
-        currentNode = self
-        while currentNode:
-            print(currentNode.data)
-            currentNode = currentNode.next
+    def dequeue(self):
+        if self.tail is None:
+            return None
         
+        if self.head == self.tail:
+            result = self.tail.data
+            self.head = None
+            self.tail = None
+            return result
+        
+        current = self.head
+        while current.next != self.tail:
+            current = current.next
+        result = self.tail.data
+        current.next = None
+        self.tail = current
+        return result
 
 
-    
+def generate_tasks(n):
+    tasks = []
+    for i in range(n):
+        if random.random() < 0.7:
+            tasks.append(('enqueue', random.randint(1, 100)))
+        else:
+            tasks.append(('dequeue', None))
+    return tasks
+
+
+def run_linkedlist_tasks(tasks):
+    queu = LinkedListQueue()
+    for i, value in tasks:
+        if i == 'enqueue':
+            queu.enqueue(value)
+        else:
+            queu.dequeue()
+
+def run_array_tasks(tasks):
+    queu = ArrayQueue()
+    for i, value in tasks:
+        if i == 'enqueue':
+            queu.enqueue(value)
+        else:
+            queu.dequeue()
+
+
+def measure_arrayqueue(tasks_lists):
+    times = []
+    for tasks in tasks_lists:
+        def time():
+            run_array_tasks(tasks)
+        t = timeit.timeit(time, number=1)
+        times.append(t)
+    return times
+
+def measure_linkedlistqueue(tasks_lists):
+    times = []
+    for tasks in tasks_lists:
+        def time():
+            run_linkedlist_tasks(tasks)
+        t = timeit.timeit(time, number=1)
+        times.append(t)
+    return times
+
 
 def main():
-    predef=[2,3,5,2,6,7,4]
-    arr= Array(predef)
-    arr.push(9)
-    arr.display()
-    print(arr.pop())
-    arr.display()
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
-    print(arr.pop())
+    num_lists = 100
+    tasks_lists = [generate_tasks(10000) for _ in range(num_lists)]
+    
+    array_time_list = measure_arrayqueue(tasks_lists)
+    linkedlist_time_list = measure_linkedlistqueue(tasks_lists)
+    
+    array_time_sorted = np.sort(array_time_list)
+    linkedlist_time_sorted = np.sort(linkedlist_time_list)
 
-    node = Node(3)
-    node = node.push(4)
-    node = node.push(5)
-    node = node.push(6)
-    node.displayLL()
-    print("---------")
-    node,data = node.pop()
-    print(data)
-    print("---------")
-    node.displayLL()
-    print("---------")
-    node,data = node.pop()
-    node,data = node.pop()
-    node,data = node.pop()
-    node,data = node.pop()
-    node,data = node.pop()
-    print("---------")
-    node.displayLL()
-
-
-
-
-
-
+    plt.figure(figsize=(10, 5))
+    plt.plot(array_time_sorted, marker='o', label='ArrayQueue')
+    plt.plot(linkedlist_time_sorted, marker='o', label='LinkedListQueue')
+    plt.xlabel('Sorted Sample')
+    plt.ylabel('Execution Time')
+    plt.title('Distribution of Execution Times')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
+
+    # Question 5 discussion
+    #  ArrayListQueue is consistently out performing LinkedListQueue.
+    #  From the slight peak on the right side we see that under poor circumstances the performance of the linked list is impacted compared to the much flatter graph for ArrayListQueue.
